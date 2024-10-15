037ae2669ec6b30c6ea9164f4da22faa1bd00e31
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-16 04:34:35 +0600

    created a simple express server

--- main.ts
+++ main.ts
@@ -1,8 +1,51 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import express from "npm:express@4.21.1";
+import {
+	Application,
+	NextFunction,
+	Request,
+	Response,
+} from "npm:@types/express";
+
+interface IErrorObject extends Error {
+	status?: number;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app: Application = express();
+
+const port = 4242;
+
+app.get("/", (_req: Request, res: Response) => {
+	res.send({
+		success: true,
+		message: "Hello Deno 2.0",
+	});
+});
+
+// Error handler for 404
+app.use((req: Request, _res: Response, next: NextFunction) => {
+	const error: IErrorObject = new Error(
+		`Requested URL '${req.url}' Not Found!`
+	);
+	error.status = 404;
+	next(error);
+});
+
+// Global Error Handler
+app.use(
+	(
+		error: IErrorObject,
+		_req: Request,
+		res: Response,
+		_next: NextFunction
+	) => {
+		console.warn(error.message);
+		res.status(error.status || 500).send({
+			success: false,
+			message: error.message || "Internal Server Error!",
+		});
+	}
+);
+
+app.listen(port, () => {
+	console.log(`Server is Running on Port: ${port}`);
+});
